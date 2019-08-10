46cc00f9432b2184bea42fe1dc21e9015c2a1af6
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2019-08-10 12:39:53 +0200

    Add Azuure hybrid identity

---
+++ microsoft-hybrid-identity-overview/03/demos/ChangeUPNs.ps1
@@ -0,0 +1,3 @@
+﻿
+$LocalUsers = Get-ADUser -Filter {UserPrincipalName -like "*contoso.local"} -Properties userPrincipalName -ResultSetSize $null
+$LocalUsers | foreach {$newUpn = $_.UserPrincipalName.Replace("contoso.local","azure.contoso.com"); $_ | Set-ADUser -UserPrincipalName $newUpn}
